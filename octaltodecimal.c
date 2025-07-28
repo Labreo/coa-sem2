9e9912e2e517e6c45bffc145da684adbc0aa177e
Labreo <74888611+Labreo@users.noreply.github.com>
2025-07-29 04:00:57 +0530

    dtoo

--- octaltodecimal.c
+++ octaltodecimal.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 int x=0;int sum=0;
-int num=0;
+int num=0;int arr[32];
 int otod(int b);
+int dtoo(int b);
 int main(){
      printf("Enter the octal no:");
     scanf("%d",&x);
     int y=otod(x);
-    printf("%d\n",y);
+    printf("The decimal no is:%d\n",y);
+    int z = dtoo(y);
+    printf("The octal no is:%d\n",z);
     return 0;
 }
 
@@ -20,4 +23,19 @@ int otod(int b){
      ++f;
     }
      return sum;
+}
+
+int dtoo(int b){
+    int k=0;int i = 0;int divide=0;
+        while(b>0){
+        divide=b%8;
+        b=b/8;
+        arr[i]=divide;
+        ++i;
+}
+int j;
+for(j=i-1;j>=0;j--){
+       k=k*10+arr[j];
+}
+return k;
 }
