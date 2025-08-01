385db8cd5b1127656212284968388e314be6a7ed
Labreo <74888611+Labreo@users.noreply.github.com>
2025-08-01 19:23:58 +0530

    Fixed octal and decimal

--- dtob.c
+++ dtob.c
@@ -1,56 +1,108 @@
 #include <stdio.h>
 #include <math.h>
-int divide=0;
-int x=0;
-int i=0;int k=0;int num=0;int f=0;int sum=0;
+int divide = 0;
+int x = 0;
+int i = 0;
+int k = 0;
+int num = 0;
+int f = 0;
 int arr[32];
 int fixed[32];
 
 int dtob(int b);
 int btod(int c);
-int main(){
-    
-    printf("Enter the decimal no:");
-    scanf("%d",&x);
-    int y=dtob(x);
-    int z=btod(y);
-    printf("%d\n",z);
-    return 0;
- }
-   /* while(x>0) {
-        divide=x%2;
-        x=x/2;
-        arr[i]=divide;
-        ++i;
-      }
+int dtoo(int b);
+int otod(int b);
+
+
+int main()
+{
+
+  printf("Enter the decimal no:");
+  scanf("%d", &x);
+  int y = dtob(x);
+  printf("Binary representation:%d\n", y);
+  int z = btod(y);
+  printf("Decimal representation:%d\n", z);
+  int a = dtoo(z);
+  printf("Octal representation:%d\n", a);
+  int b = otod(a);
+  printf("Decimal representation:%d\n", b);
+  return 0;
+}
+/* while(x>0) {
+     divide=x%2;
+     x=x/2;
+     arr[i]=divide;
+     ++i;
+   }
 int j;
-  for(j=i-1;j>=0;j--){
-    printf("%d",arr[j]);
-
-  }*/
-
-
-int dtob(int b){
-    while(b>0){
-        divide=b%2;
-        b=b/2;
-        arr[i]=divide;
-        ++i;
-}
-int j;int l;
 for(j=i-1;j>=0;j--){
-       k=k*10+arr[j];
-}
-printf("%d\n",k);
-return k;
- }
+ printf("%d",arr[j]);
 
-int btod(int c){
-     while(c>0){
-     num=c%10;
-     sum=sum+num*pow(2,f);
-     c=c/10;
-     ++f;
-    }
-     return sum;
+}*/
+int dtoo(int b)
+{
+  int k = 0;
+  int i = 0;
+  int divide = 0;
+  while (b > 0)
+  {
+    divide = b % 8;
+    b = b / 8;
+    arr[i] = divide;
+    ++i;
+  }
+  int j;
+  for (j = i - 1; j >= 0; j--)
+  {
+    k = k * 10 + arr[j];
+  }
+  return k;
+}
+
+int dtob(int b)
+{
+  while (b > 0)
+  {
+    divide = b % 2;
+    b = b / 2;
+    arr[i] = divide;
+    ++i;
+  }
+  int j;
+  int l;
+  for (j = i - 1; j >= 0; j--)
+  {
+    k = k * 10 + arr[j];
+  }
+
+  return k;
+}
+
+int btod(int c)
+{
+  int sum = 0;
+  while (c > 0)
+  {
+    num = c % 10;
+    sum = sum + num * pow(2, f);
+    c = c / 10;
+    ++f;
+  }
+  return sum;
+}
+
+int otod(int b)
+{
+  int sum = 0;
+  int f = 0;
+  while (b > 0)
+  {
+    num = b % 10;
+    sum = sum + num * pow(8, f);
+    b = b / 10;
+    ++f;
+  }
+  return sum;
 }
