98ef6ead872a032043118125a664b669a41bff5c
Labreo <74888611+Labreo@users.noreply.github.com>
2025-08-01 20:44:35 +0530

    finished exp2

--- binaryaddition.c
+++ binaryaddition.c
@@ -3,48 +3,64 @@
 void binadd(int x[4], int y[4], int result[4]);
 void compliment1(int arr[4], int result[4]);
 void compliment2(int arr[4], int result[4]);
+void binsubtract(int x[4], int y[4], int negb[4]);
 
-int main() {
+int main()
+{
     int a[4], b[4];
-    int compa[4], compb[4];
-    int answer[4] = {0, 0, 0, 0};
-
+    int answeradd[4] = {0, 0, 0, 0};
+    int answersub[4] = {0, 0, 0, 0};
     printf("Enter first number: ");
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         scanf("%d", &a[i]);
     }
 
     printf("Enter second number: ");
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         scanf("%d", &b[i]);
     }
 
-    binadd(a, b, answer);
+    binadd(a, b, answeradd);
 
     printf("\nResult (4-bit binary): ");
-    for (int i = 0; i < 4; i++) {
-        printf("%d", answer[i]);
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%d", answeradd[i]);
+    }
+
+    binsubtract(a, b, answersub);
+    printf("\nResult (4-bit binary): ");
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%d", answersub[i]);
     }
 
     return 0;
 }
 
-void binadd(int x[4], int y[4], int result[4]) {
+void binadd(int x[4], int y[4], int result[4])
+{
     int carry = 0;
-    for (int i = 3; i >= 0; i--) {
+    for (int i = 3; i >= 0; i--)
+    {
         int sum = x[i] + y[i] + carry;
         result[i] = sum % 2;
         carry = sum / 2;
     }
 }
 
-void compliment1(int arr[4], int result[4]) {
-    for (int i = 0; i < 4; i++) {
+void compliment1(int arr[4], int result[4])
+{
+    for (int i = 0; i < 4; i++)
+    {
         result[i] = arr[i] == 0 ? 1 : 0;
     }
 }
 
-void compliment2(int arr[4], int result[4]) {
+void compliment2(int arr[4], int result[4])
+{
     int one[4] = {0, 0, 0, 1};
     int temp[4];
 
@@ -52,12 +68,16 @@ void compliment2(int arr[4], int result[4]) {
     binadd(temp, one, result);
 }
 
+void binsubtract(int x[4], int y[4], int result[4])
+{
+    int negy[4];
+    compliment2(y, negy);
+    binadd(x, negy, result);
+}
 /*111+101= users enter convert dec to binary 4)additon subtraction 1's compliment 2's compliment 2's complimwnt 8 bit represemtatipn  accept 2 compliment 4bit 5 bit pass 2 compliment
-pass to 1's compliment 
-add 2 no'a funciton 
+pass to 1's compliment
+add 2 no'a funciton
 subtract 2's no
 call 2nd second pass to 2's compliment of it
 call 1's compliment in 2's compliment
-
-
 */
