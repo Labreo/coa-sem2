76bfba7dec36aaafb33ea0ab35f43fd1ff5441b8
Labreo <74888611+Labreo@users.noreply.github.com>
2025-08-01 18:39:53 +0530

    binary addition

--- binaryaddition.c
+++ binaryaddition.c
@@ -1,61 +1,64 @@
 #include <stdio.h>
 #include <math.h>
 
-int num1=0;int sum=0;int num2=0;int carry=0;int num=0;
-void binadd(int x,int y);
-void compliment1(int arr[8]);
+int carry=0;int num=0;
+void binadd(int x[4],int y[4],int result[4]);
+void compliment1(int arr[4]);
+void compliment2(int arr[4]);
 int main(){
     
-    int a[8];int b[8];
-     
+    int a[4];int b[4];
+    int result[4]={0,0,0,0};
      
     
      printf("Enter first digit:");
-    for(int i = 0; i < 8; i++) {
+    for(int i = 0; i < 4; i++) {
         scanf("%d", &a[i]);
     }
-     /*printf("Enter second digit:");
-     scanf("%d",&b);*/
-    compliment1(a);
+     printf("Enter second digit:");
+    for(int i = 0; i < 4; i++) {
+        scanf("%d", &b[i]);
+    }
+    
+    binadd(a,b,result);
+    /* compliment1(a);
     printf("\n1's Complement: ");
-    for (int i = 0; i < 8; i++) printf("%d", a[i]);
+    for (int i = 0; i < 4; i++) printf("%d", a[i]);*/
+    printf("\nResult: ");
+    for (int i = 0; i < 4; i++) printf("%d", result[i]);
     return 0;
 }
 
 
 
-/*void binadd(int x,int y){
-    num1=0;
-    sum=0;
-    num2=0;
-    i=0;
+void binadd(int x[4],int y[4],int result[4]){
     carry=0;
-    while(x>0){
-     num1=x%10;
-     x=x/10;
-     num2=y%10;
-     y=y/10;
-     if(num1+num2+carry==0){
-       sum=sum+0*pow(10,i);
+    for(int i=3;i>=0;i--){
+     if(x[i]+y[i]+carry==0){
+       result[i]=0;
         carry=0;
-     }else if(num1+num2+carry==1){
-        sum=sum+1*pow(10,i);
+     }else if(x[i]+y[i]+carry==1){
+        result[i]=1;
         carry=0;
-     }else if(num1+num2+carry==2){
-        sum=sum+0*pow(10,i);
+     }else if(x[i]+y[i]+carry==2){
+        result[i]=0;
         carry=1;
-     }else if(num1+num2+carry==3){
-        sum=sum+1*pow(10,i);
+     }else if(x[i]+y[i]+carry==3){
+        result[i]=1;
         carry=1;
      }
-     ++i;
     }
-    printf("%d",sum);
 }
-    */
+    
+void compliment2(int arr[4]){
+ for (int i = 0; i < 4; i++) {
+        arr[i] = arr[i] == 0 ? 1 : 0;
+    }
+    
+}
 
-void compliment1(int arr[8]){
- for (int i = 0; i < 8; i++) {
+void compliment1(int arr[4]){
+ for (int i = 0; i < 4; i++) {
         arr[i] = arr[i] == 0 ? 1 : 0;
     }
     
