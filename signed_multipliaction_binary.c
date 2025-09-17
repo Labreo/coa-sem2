966641692524e7667827cf7f5bd0f7b211b52d8e
Labreo <74888611+Labreo@users.noreply.github.com>
2025-09-18 00:48:31 +0530

    signed fix

--- signed_multipliaction_binary.c
+++ signed_multipliaction_binary.c
@@ -1,59 +1,78 @@
 #include <stdio.h>
+#include <string.h>
 
+void binadd(int x[4], int y[4], int result[4]);
+void binsubtract(int x[4], int y[4], int result[4]);
 void compliment1(int arr[4], int result[4]);
 void compliment2(int arr[4], int result[4]);
-void binadd(int x[4], int y[4], int result[4]);
-void ari_shift_right(int a[4], int q[4], int q1[1]);
-void binsubtract(int x[4], int y[4], int result[4]);
+void shiftleft(int a[4], int q[4]);
 
 int main() {
-    char multiplicand_str[5];
-    char multiplier_str[5];
-    int count = 4;
-
-    int m[4];                  
+    int count; 
+    int result[4];
     int a[4] = {0, 0, 0, 0};
-    int q[4];                
-    int q1[1] = {0};          
-    int result[4];             
 
-    
-    printf("Enter multiplicand (4 bits): ");
-    scanf("%s", multiplicand_str);
-    printf("Enter multiplier (4 bits): ");
-    scanf("%s", multiplier_str);
+    printf("Enter number of bits (must be 4): ");
+    scanf("%d", &count);
 
-    
-    for (int i = 0; i < 4; i++) {
-        m[i] = multiplicand_str[i] - '0';
-        q[i] = multiplier_str[i] - '0';
+    if (count != 4) {
+        printf("This program only supports 4-bit division.\n");
+        return 1;
     }
 
-   
-    while (count > 0) {
-        if (q[3] == 1 && q1[0] == 0) {
-            
-            binsubtract(a, m, result);
-            for (int i = 0; i < 4; i++) a[i] = result[i];
-        }
-        else if (q[3] == 0 && q1[0] == 1) {
-           
+    int m[4], q[4];
+    for (int i = 0; i < 4; i++) {
+        m[i] = q[i] = 0;
+    }
+
+    char input[100];
+    printf("Enter M (4 bits): ");
+    scanf("%s", input);
+    if ((int)strlen(input) != 4) {
+        printf("Invalid input. Enter exactly 4 bits.\n");
+        return 1;
+    }
+    for (int i = 0; i < 4; i++) {
+        m[i] = (input[i] == '1') ? 1 : 0;
+    }
+
+    printf("Enter Q (4 bits): ");
+    scanf("%s", input);
+    if ((int)strlen(input) != 4) {
+        printf("Invalid input. Enter exactly 4 bits.\n");
+        return 1;
+    }
+    for (int i = 0; i < 4; i++) {
+        q[i] = (input[i] == '1') ? 1 : 0;
+    }
+
+    int steps = 4;
+    while (steps != 0) {
+        shiftleft(a, q);             
+        binsubtract(a, m, result);   
+
+        for (int i = 0; i < 4; i++) a[i] = result[i];
+
+        if (a[0] == 0) {
+            q[3] = 1;
+        } else {
+         
             binadd(a, m, result);
             for (int i = 0; i < 4; i++) a[i] = result[i];
+            q[3] = 0;
         }
-       
-        ari_shift_right(a, q, q1);
-        count--;
+        --steps;
     }
 
-    printf("\nResult: ");
-    for (int i = 0; i < 4; i++) printf("%d", a[i]);
+    printf("\nQuotient (Q): ");
     for (int i = 0; i < 4; i++) printf("%d", q[i]);
-    printf("\n");
 
+    printf("\nRemainder (A): ");
+    for (int i = 0; i < 4; i++) printf("%d", a[i]);
+
+    printf("\n");
     return 0;
 }
-
 
 void compliment1(int arr[4], int result[4]) {
     for (int i = 0; i < 4; i++) {
@@ -61,14 +80,12 @@ void compliment1(int arr[4], int result[4]) {
     }
 }
 
-
 void compliment2(int arr[4], int result[4]) {
     int one[4] = {0, 0, 0, 1};
     int temp[4];
     compliment1(arr, temp);
     binadd(temp, one, result);
 }
-
 
 void binadd(int x[4], int y[4], int result[4]) {
     int carry = 0;
@@ -79,25 +96,19 @@ void binadd(int x[4], int y[4], int result[4]) {
     }
 }
 
-
-void ari_shift_right(int a[4], int q[4], int q1[1]) {
-    int sign = a[0];      
-    q1[0] = q[3];         
-
-    for (int i = 3; i > 0; i--) {
-        q[i] = q[i - 1];
-    }
-    q[0] = a[3];
-
-   
-    for (int i = 3; i > 0; i--) {
-        a[i] = a[i - 1];
-    }
-    a[0] = sign;          
-}
-
 void binsubtract(int x[4], int y[4], int result[4]) {
     int negy[4];
     compliment2(y, negy);    
     binadd(x, negy, result); 
+}
+
+void shiftleft(int a[4], int q[4]) {
+    for (int i = 0; i < 3; i++) {
+        a[i] = a[i + 1];
+    }
+    a[3] = q[0];  
+    for (int i = 0; i < 3; i++) {
+        q[i] = q[i + 1];
+    }
+    q[3] = 0;     
 }
